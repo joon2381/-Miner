4e5412fbc39b01cf27ee775e2cc0458490329328
Lee, Gyeore <lgr9603@hanmail.net>
2025-11-25 14:53:54 +0900

    Add exception handling for each func / Implementing automated crawling in progress

--- watcha_crawl_code/watcha_reviews_crawl.py
+++ watcha_crawl_code/watcha_reviews_crawl.py
@@ -38,14 +38,12 @@ def konlpy(strings, top_n=None) :
     else :
         return counter.most_common()
 
-# 단어별 빈도수 추출
-"""
-여러 문자열 리스트(strings)를 받아 자주 등장하는 단어(빈도순)와 단어별 가중치(등장 횟수)를 반환합니다.
-:param strings: 문자열의 리스트
-:param top_n: 상위 N개만 리턴하고 싶을 때 (None이면 전체)
-:return: [(단어, 등장횟수), ...] 형태의 리스트
-"""
-def get_word_frequencies(strings: list[str], top_n: int = None) -> list[tuple[str, int]]:
+
+def get_word_frequencies(strings: list[str], top_n: int = None) -> list[tuple[str, int]] :
+    # 단어별 빈도수 추출
+    """
+    여러 문자열 리스트(strings)를 받아 자주 등장하는 단어(빈도순)와 단어별 가중치(등장 횟수)를 반환합니다.
+    """
     words = []
     for s in strings:
         # 소문자로 변환 후, 단어만 추출 (영어 기준, 한글은 \w에 포함)
@@ -70,23 +68,27 @@ def delete_ad(driver: webdriver.Chrome) -> None :
         pass
 
 def watcha_open_page(driver: webdriver.Chrome) -> None :
-    # 크롬 드라이버로 네이버 열기
-    URL = 'https://naver.com'
-    driver.get(url=URL)
-    driver.implicitly_wait(100)
+    try :
+        # 크롬 드라이버로 네이버 열기
+        URL = 'https://naver.com'
+        driver.get(url=URL)
+        driver.implicitly_wait(100)
 
-    # 네이버에서 왓챠피디아 입력해서 들어가기
-    search = driver.find_element(By.CSS_SELECTOR, '#query')
-    search_content = "왓챠피디아"
-    search.send_keys(search_content)
-    driver.find_element(By.CSS_SELECTOR, 'button.btn_search').click()
-    driver.implicitly_wait(100)
-    time.sleep(1)
-    driver.find_element(By.CSS_SELECTOR,'a[href="https://pedia.watcha.com/"]').click()
-    time.sleep(1)
+        # 네이버에서 왓챠피디아 입력해서 들어가기
+        search = driver.find_element(By.CSS_SELECTOR, '#query')
+        search_content = "왓챠피디아\n"
+        search.send_keys(search_content)
+        # driver.find_element(By.CSS_SELECTOR, 'button.btn_search').click()
+        driver.implicitly_wait(100)
+        time.sleep(1)
+        driver.find_element(By.CSS_SELECTOR,'a[href="https://pedia.watcha.com/"]').click()
+        time.sleep(1)
 
-    # 탭전환
-    driver.switch_to.window(driver.window_handles[-1])
+        # 탭전환
+        driver.switch_to.window(driver.window_handles[-1])
+    except Exception as e:
+        print(f"{e} : watcha_open_page element not found or cannot interact")
+        pass
 
 def watcha_login(driver: webdriver.Chrome, USER_ID: str, USER_PWD: str) -> None :
     # 로그인
@@ -94,113 +96,149 @@ def watcha_login(driver: webdriver.Chrome, USER_ID: str, USER_PWD: str) -> None 
     왓챠피디아는 로그인하지 않은 게스트 상태로 리뷰를 볼 수 없으므로 로그인 과정 추가
     """
     delete_ad(driver)
-    driver.find_element(By.CSS_SELECTOR, 'body > main > div:nth-of-type(1) > header:nth-of-type(1) > nav > section > ul > li:nth-child(8) > button').click()
-    driver.implicitly_wait(100)
-    driver.find_element(By.CSS_SELECTOR, 'div[data-select="sign-in-dialog"] form > div:nth-of-type(1) > label > div > input').send_keys(USER_ID)
-    driver.implicitly_wait(100)
-    driver.find_element(By.CSS_SELECTOR, 'div[data-select="sign-in-dialog"] form > div:nth-of-type(2) > label > div > input').send_keys(USER_PWD+"\n")
-    time.sleep(1)
+    try :
+        login_button = driver.find_element(By.CSS_SELECTOR, 'body > main > div:nth-of-type(1) > header:nth-of-type(1) > nav > section > ul > li:nth-child(8) > button')
+        login_button.click()
+        driver.implicitly_wait(100)
+        
+        ID_input = driver.find_element(By.CSS_SELECTOR, 'div[data-select="sign-in-dialog"] form > div:nth-of-type(1) > label > div > input')
+        ID_input.send_keys(USER_ID)
+        driver.implicitly_wait(100)
 
-def watcha_search_movie(driver: webdriver.Chrome, title: str) -> str :
+        PWD_input = driver.find_element(By.CSS_SELECTOR, 'div[data-select="sign-in-dialog"] form > div:nth-of-type(2) > label > div > input')
+        PWD_input.send_keys(USER_PWD+"\n")
+        time.sleep(1)
+    except Exception as e :
+        print(f"{e} : watcha_login element not found or cannot interact")
+        pass
+    
+
+def watcha_search_movie(driver: webdriver.Chrome, title: str) -> str|None :
     # 영화 검색하기
     delete_ad(driver)
-    # body > main > div:nth-of-type(1) > header:nth-of-type(1) > nav > section > ul > li:nth-child(8) input[autocomplete="off"]
-    search = driver.find_element(By.CSS_SELECTOR, '#desktop-search-field')
-    search.send_keys(title+"\n")
-    time.sleep(1)
+    try :
+        # body > main > div:nth-of-type(1) > header:nth-of-type(1) > nav > section > ul > li:nth-child(8) input[autocomplete="off"]
+        search = driver.find_element(By.CSS_SELECTOR, '#desktop-search-field')
+        search.send_keys(title+"\n")
+        time.sleep(1)
 
-    # 영화 연도 추출
-    # body > main > div:nth-of-type(1) > section > section > div.nth-of-type(2) > div:nth-child(1) > section > section.nth-of-type(2) > div.nth-of-child(1) > ul > li > a > div.nth-of-type(2) > div.nth-of-type(2)
-    year_info = driver.find_element(By.CSS_SELECTOR,f'a[title="{MOVIE_TITLE}"] > div:nth-of-type(2) > div:nth-of-type(2)').text
-    YEAR = re.findall(r'\d{4}', year_info)[0]
+        # 영화 연도 추출
+        # body > main > div:nth-of-type(1) > section > section > div.nth-of-type(2) > div:nth-child(1) > section > section.nth-of-type(2) > div.nth-of-child(1) > ul > li > a > div.nth-of-type(2) > div.nth-of-type(2)
+        year_info = driver.find_element(By.CSS_SELECTOR,f'a[title="{MOVIE_TITLE}"] > div:nth-of-type(2) > div:nth-of-type(2)').text
+        YEAR = re.findall(r'\d{4}', year_info)[0]
 
-    # 첫 번째 검색결과 클릭하기
-    driver.find_element(By.CSS_SELECTOR,f'a[title="{title}"]').click()
-    driver.implicitly_wait(100)
+        # 첫 번째 검색결과 클릭하기
+        movie_page = driver.find_element(By.CSS_SELECTOR,f'a[title="{title}"]')
+        movie_page.click()
+        driver.implicitly_wait(100)
 
-    return YEAR
+        return YEAR
+    except Exception as e :
+        print(f"{e} : watcha_search_movie element not found or cannot interact")
+        pass
+
 
 def watcha_open_reviews(driver: webdriver.Chrome) -> None :
     # 더보기 클릭하기
     delete_ad(driver)
-    driver.find_element(By.CSS_SELECTOR,'body > main > div:nth-of-type(1) > section > div > div:nth-of-type(2) > section > section:nth-child(3) > header > div > div > a').click()
-    driver.implicitly_wait(100)
+    try :
+        review_drawer = driver.find_element(By.CSS_SELECTOR,'body > main > div:nth-of-type(1) > section > div > div:nth-of-type(2) > section > section:nth-child(3) > header > div > div > a')
+        review_drawer.click()
+        driver.implicitly_wait(100)
+    except Exception as e:
+        print(f"{e} : watcha_open_reviews element not found or cannot interact")
+        pass
 
 def watcha_load_reviews(driver: webdriver.Chrome) -> None :
-    body = driver.find_element(By.CSS_SELECTOR, 'body')
+    try :
+        body = driver.find_element(By.CSS_SELECTOR, 'body')
 
-    # 화면 스크롤 횟수 (100회 설정 시 약 1750개 리뷰 로드, 페이지 메모리 1.1GB 정도 사용)
-    COUNT = 100
+        # 화면 스크롤 횟수 (100회 설정 시 약 1750개 리뷰 로드, 페이지 메모리 1.1GB 정도 사용)
+        COUNT = 100
 
-    for i in range(COUNT) :
-        # body 태그에 END 키를 입력하는 것으로 페이지 최하단으로 스크롤
-        body.send_keys(Keys.END)
-        # 원활한 페이지 로딩을 위한 멈춤
-        time.sleep(0.5)
+        for i in range(COUNT) :
+            # body 태그에 END 키를 입력하는 것으로 페이지 최하단으로 스크롤
+            body.send_keys(Keys.END)
+            # 원활한 페이지 로딩을 위한 멈춤
+            time.sleep(0.5)
+    except Exception as e:
+        print(f"{e} : watcha_load_reviews element not found or cannot interact")
+        pass
 
-def watcha_extract_reviews(driver: webdriver.Chrome) -> pd.DataFrame :
+def watcha_extract_reviews(driver: webdriver.Chrome) -> pd.DataFrame|None :
     # 리뷰 작성자 및 내용 추출
     """
+    rating -> 리뷰에 대한 평점이 담긴 html 태그를 iterable한 객체(List)로 저장
     reviewers -> 리뷰를 작성한 유저명이 담긴 html 태그를 iterable한 객체(List)로 저장
     reviews -> 리뷰의 내용이 담긴 html 태그를 iterable한 객체(List)로 저장
+
+    이후 각 정보를 pandas DataFrame 형태로 변환(merge)하여 return
     """
     reviews, reviewers, rating = [], [], []
+    try :
+        # List 새로고침
+        rating = driver.find_elements(By.CSS_SELECTOR, 'body > main > div > section > div:nth-of-type(2) li:nth-of-type(n) > article > a:nth-of-type(1) > header > div:nth-of-type(2) > p')
+        reviewers = driver.find_elements(By.CSS_SELECTOR, 'body > main > div > section > div:nth-of-type(2) li:nth-of-type(n) > article > a:nth-of-type(1) > header > div:nth-of-type(1) > p')
+        reviews = driver.find_elements(By.CSS_SELECTOR, 'body > main > div > section > div:nth-of-type(2) li:nth-of-type(n) > article > a:nth-of-type(2) > p')
 
-    # List 새로고침
-    rating = driver.find_elements(By.CSS_SELECTOR, 'body > main > div > section > div:nth-of-type(2) li:nth-of-type(n) > article > a:nth-of-type(1) > header > div:nth-of-type(2) > p')
-    reviewers = driver.find_elements(By.CSS_SELECTOR, 'body > main > div > section > div:nth-of-type(2) li:nth-of-type(n) > article > a:nth-of-type(1) > header > div:nth-of-type(1) > p')
-    reviews = driver.find_elements(By.CSS_SELECTOR, 'body > main > div > section > div:nth-of-type(2) li:nth-of-type(n) > article > a:nth-of-type(2) > p')
+        # pandas dataframe
+        data = []
+        for user, rev, rat in tqdm(zip(reviewers, reviews, rating), desc=f"Constructing DataFrame : {MOVIE_TITLE}", mininterval=1, total=len(reviews)) :
+            username = user.text
+            review_text = rev.text.replace("\n"," ")
+            review_len = len(review_text)
+            rating_value = float(rat.text)
+            data.append({"site": "watcha", "title": MOVIE_TITLE, "title_en": MOVIE_TITLE_EN, "year": MOVIE_YEAR, "reviewer": username, "review": review_text, "review_length": review_len, "rating": rating_value})
+        df = pd.DataFrame(data)
 
-    # pandas dataframe
-    data = []
-    for user, rev, rat in tqdm(zip(reviewers, reviews, rating), desc="Constructing DataFrame", mininterval=1):
-        username = user.text
-        review_text = rev.text.replace("\n"," ")
-        review_len = len(review_text)
-        rating_value = float(rat.text)
-        data.append({"site": "watcha", "title": MOVIE_TITLE, "title_en": MOVIE_TITLE_EN, "year": MOVIE_YEAR, "reviewer": username, "review": review_text, "review_length": review_len, "rating": rating_value})
-    df = pd.DataFrame(data)
-
-    return df
-
-
-driver = webdriver.Chrome()
-
-watcha_open_page(driver)
+        return df
+    except Exception as e:
+        print(f"{e} : watcha_extract_reviews element not found or cannot interact")
+        pass
 
 # 왓챠피디아 아이디
 USER_ID = "lgr9603@kangwon.ac.kr"
 USER_PWD = "lgr2618409!"
-
-watcha_login(driver, USER_ID, USER_PWD)
 
 # 검색할 영화명
 """
 ** 주의점 **
 영화명을 검색한 뒤 가장 처음 검색 결과로 나온 영화의 리뷰를 크롤링하므로 영화 제목의 오탈자에 주의할 것
 """
-MOVIE_TITLE = "폭싹 속았수다"
-MOVIE_TITLE_EN = "When Life Gives You Tangerines"
+# MOVIE_TITLE = "폭싹 속았수다"
+# MOVIE_TITLE_EN = "When Life Gives You Tangerines"
 
-MOVIE_YEAR = watcha_search_movie(driver, MOVIE_TITLE)
+MOVIE_TITLE_LIST = ["폭싹 속았수다"]
+MOVIE_TITLE_EN_LIST = ["WhenLifeGivesYouTangerines"]
 
-watcha_open_reviews(driver)
+driver = webdriver.Chrome()
 
-watcha_load_reviews(driver)
+for MOVIE_TITLE, MOVIE_TITLE_EN in tqdm(zip(MOVIE_TITLE_LIST, MOVIE_TITLE_EN_LIST), desc="Total Progress", mininterval=10, total=len(MOVIE_TITLE_LIST)) :
+    watcha_open_page(driver)
 
-df = watcha_extract_reviews(driver)
+    watcha_login(driver, USER_ID, USER_PWD)
 
-print(df)
-print(get_word_frequencies(df["review"], top_n=20))
-print(konlpy(df["review"], top_n=20))
+    MOVIE_YEAR = watcha_search_movie(driver, MOVIE_TITLE)
 
-df.to_csv("watcha_korean_reviews_minimal.csv", index=False)
+    watcha_open_reviews(driver)
+
+    watcha_load_reviews(driver)
+
+    df = watcha_extract_reviews(driver)
+
+    print(df)
+    print(get_word_frequencies(df["review"], top_n=20))
+    print(konlpy(df["review"], top_n=20))
+
+    df.to_csv(f"watcha_korean_{MOVIE_TITLE_EN}_reviews_minimal.csv", index=False)
 
 # Wait for user input for termination
 input("Press Enter to terminate...")
 driver.quit()
 
-#TODO: 영화 제목을 리스트로 구성, 현재 main 부분을 함수화하여 for문으로 여러 영화 크롤링 가능하게 하기
+#TODO: 영화 제목을 리스트로 구성, 현재 main 부분을 함수화하여 for문으로 여러 영화 크롤링 가능하게 하기 **NOT DONE YET**
+#TODO: CSS Selector 예외처리 및 각 선택자 변수지정 **DONE**
 #TODO: 리뷰 내용 중 비언어적 문자(ex: emoji) 삭제 및 리뷰 내용의 길이에 따라 필터링 기능 추가하기
+#TODO: 리뷰에 스포일러 방지가 있는 경우 텍스트가 표시되지 않음. 이 경우 스포일러 방지 해제 또는 크롤링에서 제외하기
 #TODO: 영화별로 크롤링 결과를 다른 파일에 저장하고, 각 파일명에 영화 제목 포함시키기
 #TODO: 가능하다면 리뷰 작성 일자도 크롤링하기
